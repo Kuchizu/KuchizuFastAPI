3e05878d0d907ea68c758ac01327f1c342e0f234
Kuchizu <legolashacker0@gmail.com>
2022-01-13 21:59:36 +0500

    .

--- Kuchizu.py
+++ Kuchizu.py
@@ -2,16 +2,17 @@ import os
 import logging
 from random import choice
 from fastapi import FastAPI
-from fastapi.responses import FileResponse
-from pydantic import BaseModel
+from fastapi.responses import PlainTextResponse, FileResponse
+from starlette.exceptions import HTTPException
 
-logging.basicConfig(level = logging.INFO)
+logging.basicConfig(level=logging.INFO)
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
-with open('cats.txt', encoding = 'UTF-8') as f:
+with open('files/cats.txt', encoding = 'UTF-8') as f:
     cats = f.read().split()
 
 app = FastAPI()
 
+# from pydantic import BaseModel
 # class Data(BaseModel):
 #     obj: str
 #     # smth: int | None # Python 3.10 ?
@@ -20,10 +21,17 @@ app = FastAPI()
 async def root():
     return {'Neko': choice(cats)}
 
+@app.exception_handler(HTTPException)
+async def notfound(request, exc):
+    return PlainTextResponse(f'404 Not Found.')
+
 @app.get('/favicon.ico')
 async def icon():
-    return FileResponse('Kuchizu.ico')
+    return FileResponse('icons/Kuchizu.ico')
 
-@app.get('/Pico.m4a')
-async def icon():
-    return FileResponse('Boku no pico.m4a')
+@app.get('/files/{file}')
+async def get_file(file):
+    if os.path.exists(f'files/{file}'):
+        return FileResponse(f'files/{file}')
+    else:
+        return PlainTextResponse('403 Forbidden.')
