40a6c56eeeba8ecddd421ef8dc6f9e4c7474d669
Kuchizu <legolashacker0@gmail.com>
2022-01-31 00:04:03 +0500

    Bruh

--- Kuchizu.py
+++ Kuchizu.py
@@ -1,13 +1,13 @@
 import os
 import logging
 from random import choice
-from fastapi import FastAPI
-from fastapi.responses import PlainTextResponse, FileResponse
+from fastapi import FastAPI, Request
+from fastapi.responses import PlainTextResponse, FileResponse, HTMLResponse
 from starlette.exceptions import HTTPException
 
 logging.basicConfig(level=logging.INFO)
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
-with open('files/cats.txt', encoding = 'UTF-8') as f:
+with open('files/Cats.txt', encoding = 'UTF-8') as f:
     cats = f.read().split()
 
 app = FastAPI()
@@ -28,6 +28,19 @@ async def notfound(request, exc):
 @app.get('/favicon.ico')
 async def icon():
     return FileResponse('icons/Kuchizu.ico')
+
+@app.get('/files')
+async def get_files(request: Request):
+    html = f"Founded {len(next(os.walk('files'))[2])} files:<br><br>"
+    for root, dirs, files in os.walk('files'):
+        for filename in files:
+            html += '<a href=\"{link}\">{title}</a>  {filesize}<br>'.format(
+                link = f'{request.url._url}/{filename}',
+                title = filename,
+                filesize = f'{os.path.getsize(os.path.join(root, filename)) / 1024 / 1024:.2f}MB'
+            )
+    return HTMLResponse(html)
+
 
 @app.get('/files/{file}')
 async def get_file(file):
